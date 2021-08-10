b53ac61703d001e4129b694d6b4c5585e5c30a5c
gaytan-aaron <aaron.gaytan@comunidad.unam.mx>
2021-08-10 03:48:57 -0500

    AplicacionLista v1.03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import { printExpression } from './js/componentes';
 
 console.log( { printExpression } );
